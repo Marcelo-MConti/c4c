8e5c5f0f062afbe00580ce5746b3768e31435fd1
goll <8541473-goll72@users.noreply.gitlab.com>
2022-04-02 18:34:02 -0300

    Finish ASCII support

--- include/chars.h
+++ include/chars.h
@@ -5,17 +5,13 @@
 
 #ifdef C4C_ASCII
 const static char *arrow = "\\/";
-
 const static char *checkers[] = {
     [NONE] = " ", "#", "O"
 };
 #else
-/* Right block diagonal (U+1FB65) + Left block diagonal (U+1FB5A) */
-const static char *arrow = "\360\237\255\245\360\237\255\232";
-
-/* Fisheye character */
+const static char *arrow = u8"🭥🭚";
 const static char *checkers[] = {
-    [NONE] = " ", "\342\227\211", "\342\227\211"
+    [NONE] = " ", u8"◉", u8"◉"
 };
 #endif
 
